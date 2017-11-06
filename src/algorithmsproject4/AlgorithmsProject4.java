b9ee2485d822fee70f9790ea2c148a00b29c4189
JamesOsborne <james.osborne112@gmail.com>
2017-11-06 09:33:46 -0500

    Fix spacing

--- src/algorithmsproject4/AlgorithmsProject4.java
+++ src/algorithmsproject4/AlgorithmsProject4.java
@@ -6,6 +6,7 @@ public class AlgorithmsProject4 {
     public static void main(String[] args) throws FileNotFoundException {
         String input = args[0];
         Solver rushHourSolver = new Solver(input);
+        
         rushHourSolver.solve();
     }
 }
