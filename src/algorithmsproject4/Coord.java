f853e311a71c13cc4cd307754a8fda00d2a535ce
JamesOsborne <james.osborne112@gmail.com>
2017-11-06 09:29:17 -0500

    Create coord class to house x and y coordinates

---
+++ src/algorithmsproject4/Coord.java
@@ -0,0 +1,27 @@
+package algorithmsproject4;
+
+public class Coord {
+    private int x;
+    private int y;
+
+    public Coord(int xLoc, int yLoc) {
+        this.x = xLoc;
+        this.y = yLoc;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int xLoc)  {
+        this.x = xLoc;
+    }
+
+    public void setY(int yLoc)  {
+        this.y = yLoc;
+    }
+}
